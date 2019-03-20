f7df44083cd7e7088d3672b345dc97182c356649
Александр <pyankov.work@gmail.com>
2019-03-20 10:56:38 +0300

    Ex 10.3 Done

--- Exersize_10_3/Program.cs
+++ Exersize_10_3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Exersize_10_3
@@ -10,6 +11,18 @@ namespace Exersize_10_3
     {
         static void Main(string[] args)
         {
+            var source = new StreamReader("source.txt", Encoding.Default);
+            var result = new StreamWriter("result.txt", false, Encoding.Default);
+
+            using (result)
+            using (source)
+                while (!source.EndOfStream)
+                {
+                    string line = source.ReadLine();
+                    int countSpec = line.Count(c => !Char.IsLetter(c) && !Char.IsNumber(c));
+                    if (countSpec > 0)
+                        result.WriteLine($"{line} - {countSpec}");
+                }
         }
     }
 }
