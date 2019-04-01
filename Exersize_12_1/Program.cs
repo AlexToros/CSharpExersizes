1cb0fc65a63cf7a37b093d6424e08fa899293260
Александр <pyankov.work@gmail.com>
2019-04-01 12:24:26 +0300

    Ex 12 Done

---
+++ Exersize_12_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersize_12_1
+{
+    delegate void Binnary(double arg1, ref double arg2);
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите X: ");
+            double X = double.Parse(Console.ReadLine());
+            Console.Write("Введите A: ");
+            double A = double.Parse(Console.ReadLine());
+            double xCopy = X;
+            double controlX = Math.Pow((A / Math.Pow(X, A) + A) * A - A, A);
+
+            Binnary CalcY;
+            Binnary Mul = delegate (double x, ref double y) { y *= x; };
+            Binnary Div = delegate (double x, ref double y) { y = x / y; };
+            Binnary Add = delegate (double x, ref double y) { y += x; };
+            Binnary Sub = delegate (double x, ref double y) { y = y - x; };
+            Binnary Pow = delegate (double x, ref double y) { y = Math.Pow(y, x); };
+
+            CalcY = Pow;
+            CalcY += Div;
+            CalcY += Add;
+            CalcY += Mul;
+            CalcY += Sub;
+            CalcY += Pow;
+
+            CalcY.Invoke(A, ref xCopy);
+
+            Console.WriteLine("Проверка с использованием анонимных методов: " + (xCopy == controlX));
+
+            xCopy = X;
+
+            Mul = (double x, ref double y) => y *= x; 
+            Div = (double x, ref double y) => y = x / y;
+            Add = (double x, ref double y) => y += x;
+            Sub = (double x, ref double y) => y = y - x;
+            Pow = (double x, ref double y) => y = Math.Pow(y, x); 
+
+            CalcY = Pow;
+            CalcY += Div;
+            CalcY += Add;
+            CalcY += Mul;
+            CalcY += Sub;
+            CalcY += Pow;
+
+            CalcY.Invoke(A, ref xCopy);
+
+            Console.WriteLine("Проверка с использованием блочных лямбда выражений: " + (xCopy == controlX));
+        }
+    }
+}
---
+++ Exersize_12_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersize_12_2
+{
+    delegate bool BoleanF(double x);
+    delegate double Unary(double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoleanF PowOfTwo = x => {
+                double temp = Math.Log(x, 2);
+                return Math.Abs(temp - (int)temp) < double.Epsilon;
+            };
+            Unary Fact = x =>
+            {
+                double t = 1;
+                for (int i = 2; i <= x; i++)
+                    t *= i;
+                return t;
+            };
+            Unary Reciprocal = x => 1 / x;
+            Unary FracPart = x => x - Math.Round(x);
+            BoleanF IsEven = x => x % 2 == 0;
+            BoleanF isOdd = x => !IsEven(x);
+            Unary Crt = x => Math.Pow(x, 1.0 / 3);
+            Unary DegToRad = x => x * Math.PI / 180;
+            Unary RadToDeg = x => x * 180 / Math.PI;
+        }
+    }
+}
